c703e881d19cf1a393a79bbd9152353b7e4ded3a
Nurvirta Monarizqa <nmonarizqa@gmail.com>
2017-07-25 01:05:23 -0400

    add html page

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request, url_for, abort
 from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
-
-    return """
-    <h1>Your Beauty Guru Recommendation</h1>
-    <p>This app is in progress. Here are the initial explorations</p>
-    """.format(time=the_time)
+    # the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+    #
+    # return """
+    # <h1>Your Beauty Guru Recommendation</h1>
+    # <p>This app is in progress. Here are the initial explorations</p>
+    # """
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
